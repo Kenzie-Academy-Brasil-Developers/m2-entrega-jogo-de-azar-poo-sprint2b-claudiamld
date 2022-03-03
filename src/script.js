562aab9a7be702c60eca2ff9373f4d64fe2dcdd1
Claudia Dias <claudiamld@outlook.com>
2022-03-03 11:52:55 -0300

    jogo finalizado

--- src/script.js
+++ src/script.js
@@ -1,1 +1,38 @@
-// Escreva seu código aqui
+const btn = document.querySelector('button')
+const body = document.body.querySelector('body')
+
+const mensagens = [
+    'Você ainda não está preparado para isso!', 
+    'Isso irá acontecer em breve!',
+    'Desista enquanto há tempo!',
+    'No fundo, você sabe a resposta'
+]
+
+function textoMensagem(){
+    const random = Math.floor(Math.random() * (4 - 0) + 0 )
+    console.log(random);
+    switch (random) {
+        case 0:
+            return `${mensagens[0]}`
+            break;
+        case 1:
+            return `${mensagens[1]}`
+            break;
+        case 2:
+            return `${mensagens[2]}`
+            break;
+        case 3:
+            return `${mensagens[3]}`
+            break;
+    }
+}
+
+function renderizarMensagem(){
+    const divMensagem = document.querySelector('#mensagem')
+    divMensagem.innerHTML = ''
+    const span = document.createElement('span')
+    span.innerText = textoMensagem()
+    divMensagem.appendChild(span)
+}
+
+btn.addEventListener('click', renderizarMensagem)
